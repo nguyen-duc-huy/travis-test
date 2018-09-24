573cb8b5a562052dd09bf2dd91258efd727ccde7
Nguyen Duc Huy <huyduc.nguyen@bluecomgroup.com>
2018-09-24 11:50:18 +0700

    Use $COMPOSER_HOME

--- scan-code.sh
+++ scan-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PHPCS_PATH=$HOME/.config/composer/vendor/squizlabs/php_codesniffer/bin/phpcs
+PHPCS_PATH=$COMPOSER_HOME/vendor/squizlabs/php_codesniffer/bin/phpcs
 STANDARD_PATH=$TRAVIS_BUILD_DIR/ruleset.xml
 SCAN_PATH=test.php
 
