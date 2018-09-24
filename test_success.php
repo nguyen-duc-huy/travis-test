8fa4bc9b106c24ac9c5270c7f28d19dd8135521c
Nguyen Duc Huy <huyduc.nguyen@bluecomgroup.com>
2018-09-24 12:21:47 +0700

    Add a test success php file, add NAME env for scripts

---
+++ test_success.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HelloWorld;
+
+class Testclass
+{
+    /**
+     * @param string $a
+     * @return $a
+     */
+    public function helloWorld($a)
+    {
+        if ($a) {
+            return 'asd';
+        }
+
+        return 1;
+    }
+}
